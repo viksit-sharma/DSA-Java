f860e71a8ed3d7756c78cf6e4969b2e035fcaba2
viksit sharma <viksitsharmapvt@gmail.com>
2025-11-03 11:25:38 +0530

    college java

---
+++ java_clg/checkPalid.java
@@ -0,0 +1,12 @@
+public class checkPalid {
+    public static void main(String[] args) {
+        String str = "MalayalaM";
+        for (int i = 0; i < 9; i++) {
+            if (str.charAt(i) != str.charAt(8-i)) {
+                System.out.println("Is not palindrome");
+                return;
+            }
+        }
+        System.out.println("Is palindrome");
+    }
+}
