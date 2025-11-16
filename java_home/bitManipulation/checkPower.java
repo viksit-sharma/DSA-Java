e9503d8ff2a0aedcfaa3558666ce0ac96026a89b
viksit sharma <viksitsharmapvt@gmail.com>
2025-11-17 03:01:20 +0530

    completed objects and classes

--- java_home/bitManipulation/checkPower.java
+++ java_home/bitManipulation/checkPower.java
@@ -1,23 +1,23 @@
 package bitManipulation;
 
 public class checkPower {
-    public static void check2toThePowerMine(int num) {//NOT WORKING
-        int copy = num;
-        for (int i = 0; i < copy; i++) {
-            if (num == (int) Math.pow(2, i)) {
-                System.out.println("Yes, the no is " + i + "th power of 2");
-                return;
-            }
-            System.out.println(i);
-            copy /= 2;
-        }
-        System.out.println("No, the no is not in the power of 2");
-    }
+    // public static void check2toThePowerMine(int num) {//NOT WORKING
+    //     int copy = num;
+    //     for (int i = 0; i < copy; i++) {
+    //         if (num == (int) Math.pow(2, i)) {
+    //             System.out.println("Yes, the no is " + i + "th power of 2");
+    //             return;
+    //         }
+    //         System.out.println(i);
+    //         copy /= 2;
+    //     }
+    //     System.out.println("No, the no is not in the power of 2");
+    // }
     public static boolean check2toThePower(int num) {
         return ((num & (num - 1)) == 0);//1000 & 0111 == 0 means num is in power of two
     }
     public static void main(String[] args) {
-        check2toThePowerMine(16);
+        check2toThePower(16);
         // if (num == true) {
         //     System.out.println("Is in power of two");
         // }
